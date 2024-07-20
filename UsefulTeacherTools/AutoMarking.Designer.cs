d34be7f7035a1b6cb6ea1a29b2b554ae62599b24
Ayan Karzhaubay <ayan.karzhaubay@gmail.com>
2024-07-20 22:20:31 +0500

    Добавьте файлы проекта.

---
+++ UsefulTeacherTools/AutoMarking.Designer.cs
@@ -0,0 +1,374 @@
+﻿namespace UsefulTeacherTools
+{
+    partial class AutoMarking
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonCalculate = new System.Windows.Forms.Button();
+            this.textBoxMaxMark = new System.Windows.Forms.TextBox();
+            this.labelMaxScore = new System.Windows.Forms.Label();
+            this.labelPupil = new System.Windows.Forms.Label();
+            this.labelScore = new System.Windows.Forms.Label();
+            this.textBoxFiveCount = new System.Windows.Forms.TextBox();
+            this.textBoxFourCount = new System.Windows.Forms.TextBox();
+            this.textBoxThreeCount = new System.Windows.Forms.TextBox();
+            this.textBoxClass = new System.Windows.Forms.TextBox();
+            this.textBoxMarks = new System.Windows.Forms.TextBox();
+            this.textBoxThree = new System.Windows.Forms.TextBox();
+            this.textBoxFive = new System.Windows.Forms.TextBox();
+            this.textBoxFour = new System.Windows.Forms.TextBox();
+            this.labelQuality = new System.Windows.Forms.Label();
+            this.textBoxQuality = new System.Windows.Forms.TextBox();
+            this.tabControlMain = new System.Windows.Forms.TabControl();
+            this.tabPageMain = new System.Windows.Forms.TabPage();
+            this.labelFive = new System.Windows.Forms.Label();
+            this.labelFour = new System.Windows.Forms.Label();
+            this.labelThree = new System.Windows.Forms.Label();
+            this.tabPageAbout = new System.Windows.Forms.TabPage();
+            this.labelRequisites = new System.Windows.Forms.Label();
+            this.labelSupport = new System.Windows.Forms.Label();
+            this.labelAuthor = new System.Windows.Forms.Label();
+            this.buttonClear = new System.Windows.Forms.Button();
+            this.tabControlMain.SuspendLayout();
+            this.tabPageMain.SuspendLayout();
+            this.tabPageAbout.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // buttonCalculate
+            // 
+            this.buttonCalculate.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.buttonCalculate.Location = new System.Drawing.Point(418, 356);
+            this.buttonCalculate.Name = "buttonCalculate";
+            this.buttonCalculate.Size = new System.Drawing.Size(186, 45);
+            this.buttonCalculate.TabIndex = 10;
+            this.buttonCalculate.Text = "Есептеу";
+            this.buttonCalculate.UseVisualStyleBackColor = true;
+            this.buttonCalculate.Click += new System.EventHandler(this.buttonCalculate_Click);
+            // 
+            // textBoxMaxMark
+            // 
+            this.textBoxMaxMark.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBoxMaxMark.Location = new System.Drawing.Point(224, 15);
+            this.textBoxMaxMark.Name = "textBoxMaxMark";
+            this.textBoxMaxMark.Size = new System.Drawing.Size(179, 34);
+            this.textBoxMaxMark.TabIndex = 11;
+            // 
+            // labelMaxScore
+            // 
+            this.labelMaxScore.AutoSize = true;
+            this.labelMaxScore.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.labelMaxScore.Location = new System.Drawing.Point(17, 18);
+            this.labelMaxScore.Name = "labelMaxScore";
+            this.labelMaxScore.Size = new System.Drawing.Size(201, 28);
+            this.labelMaxScore.TabIndex = 12;
+            this.labelMaxScore.Text = "Максималды балл:";
+            // 
+            // labelPupil
+            // 
+            this.labelPupil.AutoSize = true;
+            this.labelPupil.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.labelPupil.Location = new System.Drawing.Point(17, 61);
+            this.labelPupil.Name = "labelPupil";
+            this.labelPupil.Size = new System.Drawing.Size(83, 28);
+            this.labelPupil.TabIndex = 15;
+            this.labelPupil.Text = "Оқушы";
+            // 
+            // labelScore
+            // 
+            this.labelScore.AutoSize = true;
+            this.labelScore.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.labelScore.Location = new System.Drawing.Point(323, 61);
+            this.labelScore.Name = "labelScore";
+            this.labelScore.Size = new System.Drawing.Size(60, 28);
+            this.labelScore.TabIndex = 16;
+            this.labelScore.Text = "Балл";
+            // 
+            // textBoxFiveCount
+            // 
+            this.textBoxFiveCount.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBoxFiveCount.Location = new System.Drawing.Point(754, 647);
+            this.textBoxFiveCount.Name = "textBoxFiveCount";
+            this.textBoxFiveCount.ReadOnly = true;
+            this.textBoxFiveCount.Size = new System.Drawing.Size(175, 34);
+            this.textBoxFiveCount.TabIndex = 9;
+            // 
+            // textBoxFourCount
+            // 
+            this.textBoxFourCount.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBoxFourCount.Location = new System.Drawing.Point(754, 356);
+            this.textBoxFourCount.Name = "textBoxFourCount";
+            this.textBoxFourCount.ReadOnly = true;
+            this.textBoxFourCount.Size = new System.Drawing.Size(175, 34);
+            this.textBoxFourCount.TabIndex = 8;
+            // 
+            // textBoxThreeCount
+            // 
+            this.textBoxThreeCount.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBoxThreeCount.Location = new System.Drawing.Point(754, 65);
+            this.textBoxThreeCount.Name = "textBoxThreeCount";
+            this.textBoxThreeCount.ReadOnly = true;
+            this.textBoxThreeCount.Size = new System.Drawing.Size(175, 34);
+            this.textBoxThreeCount.TabIndex = 3;
+            // 
+            // textBoxClass
+            // 
+            this.textBoxClass.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBoxClass.Location = new System.Drawing.Point(17, 92);
+            this.textBoxClass.Multiline = true;
+            this.textBoxClass.Name = "textBoxClass";
+            this.textBoxClass.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBoxClass.Size = new System.Drawing.Size(300, 840);
+            this.textBoxClass.TabIndex = 17;
+            // 
+            // textBoxMarks
+            // 
+            this.textBoxMarks.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBoxMarks.Location = new System.Drawing.Point(323, 92);
+            this.textBoxMarks.Multiline = true;
+            this.textBoxMarks.Name = "textBoxMarks";
+            this.textBoxMarks.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBoxMarks.Size = new System.Drawing.Size(80, 840);
+            this.textBoxMarks.TabIndex = 18;
+            // 
+            // textBoxThree
+            // 
+            this.textBoxThree.Location = new System.Drawing.Point(629, 105);
+            this.textBoxThree.Multiline = true;
+            this.textBoxThree.Name = "textBoxThree";
+            this.textBoxThree.ReadOnly = true;
+            this.textBoxThree.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBoxThree.Size = new System.Drawing.Size(300, 245);
+            this.textBoxThree.TabIndex = 21;
+            // 
+            // textBoxFive
+            // 
+            this.textBoxFive.Location = new System.Drawing.Point(629, 687);
+            this.textBoxFive.Multiline = true;
+            this.textBoxFive.Name = "textBoxFive";
+            this.textBoxFive.ReadOnly = true;
+            this.textBoxFive.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBoxFive.Size = new System.Drawing.Size(300, 245);
+            this.textBoxFive.TabIndex = 22;
+            // 
+            // textBoxFour
+            // 
+            this.textBoxFour.Location = new System.Drawing.Point(629, 396);
+            this.textBoxFour.Multiline = true;
+            this.textBoxFour.Name = "textBoxFour";
+            this.textBoxFour.ReadOnly = true;
+            this.textBoxFour.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBoxFour.Size = new System.Drawing.Size(300, 245);
+            this.textBoxFour.TabIndex = 23;
+            // 
+            // labelQuality
+            // 
+            this.labelQuality.AutoSize = true;
+            this.labelQuality.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.labelQuality.Location = new System.Drawing.Point(629, 21);
+            this.labelQuality.Name = "labelQuality";
+            this.labelQuality.Size = new System.Drawing.Size(91, 28);
+            this.labelQuality.TabIndex = 26;
+            this.labelQuality.Text = "Сапа, %:";
+            // 
+            // textBoxQuality
+            // 
+            this.textBoxQuality.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBoxQuality.Location = new System.Drawing.Point(754, 18);
+            this.textBoxQuality.Name = "textBoxQuality";
+            this.textBoxQuality.ReadOnly = true;
+            this.textBoxQuality.Size = new System.Drawing.Size(175, 34);
+            this.textBoxQuality.TabIndex = 25;
+            // 
+            // tabControlMain
+            // 
+            this.tabControlMain.Controls.Add(this.tabPageMain);
+            this.tabControlMain.Controls.Add(this.tabPageAbout);
+            this.tabControlMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControlMain.Location = new System.Drawing.Point(0, 0);
+            this.tabControlMain.Name = "tabControlMain";
+            this.tabControlMain.SelectedIndex = 0;
+            this.tabControlMain.Size = new System.Drawing.Size(1016, 979);
+            this.tabControlMain.TabIndex = 27;
+            // 
+            // tabPageMain
+            // 
+            this.tabPageMain.Controls.Add(this.buttonClear);
+            this.tabPageMain.Controls.Add(this.labelFive);
+            this.tabPageMain.Controls.Add(this.labelFour);
+            this.tabPageMain.Controls.Add(this.labelThree);
+            this.tabPageMain.Controls.Add(this.textBoxClass);
+            this.tabPageMain.Controls.Add(this.labelQuality);
+            this.tabPageMain.Controls.Add(this.textBoxThreeCount);
+            this.tabPageMain.Controls.Add(this.textBoxQuality);
+            this.tabPageMain.Controls.Add(this.textBoxFourCount);
+            this.tabPageMain.Controls.Add(this.textBoxFour);
+            this.tabPageMain.Controls.Add(this.textBoxFiveCount);
+            this.tabPageMain.Controls.Add(this.textBoxFive);
+            this.tabPageMain.Controls.Add(this.buttonCalculate);
+            this.tabPageMain.Controls.Add(this.textBoxThree);
+            this.tabPageMain.Controls.Add(this.textBoxMaxMark);
+            this.tabPageMain.Controls.Add(this.textBoxMarks);
+            this.tabPageMain.Controls.Add(this.labelMaxScore);
+            this.tabPageMain.Controls.Add(this.labelPupil);
+            this.tabPageMain.Controls.Add(this.labelScore);
+            this.tabPageMain.Location = new System.Drawing.Point(4, 29);
+            this.tabPageMain.Name = "tabPageMain";
+            this.tabPageMain.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPageMain.Size = new System.Drawing.Size(1008, 946);
+            this.tabPageMain.TabIndex = 0;
+            this.tabPageMain.Text = "Басты бет";
+            this.tabPageMain.UseVisualStyleBackColor = true;
+            // 
+            // labelFive
+            // 
+            this.labelFive.AutoSize = true;
+            this.labelFive.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.labelFive.Location = new System.Drawing.Point(629, 650);
+            this.labelFive.Name = "labelFive";
+            this.labelFive.Size = new System.Drawing.Size(119, 28);
+            this.labelFive.TabIndex = 29;
+            this.labelFive.Text = "85%-100%:";
+            // 
+            // labelFour
+            // 
+            this.labelFour.AutoSize = true;
+            this.labelFour.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.labelFour.Location = new System.Drawing.Point(629, 359);
+            this.labelFour.Name = "labelFour";
+            this.labelFour.Size = new System.Drawing.Size(107, 28);
+            this.labelFour.TabIndex = 28;
+            this.labelFour.Text = "64%-85%:";
+            // 
+            // labelThree
+            // 
+            this.labelThree.AutoSize = true;
+            this.labelThree.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.labelThree.Location = new System.Drawing.Point(629, 68);
+            this.labelThree.Name = "labelThree";
+            this.labelThree.Size = new System.Drawing.Size(107, 28);
+            this.labelThree.TabIndex = 27;
+            this.labelThree.Text = "40%-64%:";
+            // 
+            // tabPageAbout
+            // 
+            this.tabPageAbout.Controls.Add(this.labelRequisites);
+            this.tabPageAbout.Controls.Add(this.labelSupport);
+            this.tabPageAbout.Controls.Add(this.labelAuthor);
+            this.tabPageAbout.Location = new System.Drawing.Point(4, 29);
+            this.tabPageAbout.Name = "tabPageAbout";
+            this.tabPageAbout.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPageAbout.Size = new System.Drawing.Size(1008, 946);
+            this.tabPageAbout.TabIndex = 1;
+            this.tabPageAbout.Text = "Байланыс";
+            this.tabPageAbout.UseVisualStyleBackColor = true;
+            // 
+            // labelRequisites
+            // 
+            this.labelRequisites.AutoSize = true;
+            this.labelRequisites.Font = new System.Drawing.Font("Times New Roman", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.labelRequisites.Location = new System.Drawing.Point(28, 173);
+            this.labelRequisites.Name = "labelRequisites";
+            this.labelRequisites.Size = new System.Drawing.Size(445, 45);
+            this.labelRequisites.TabIndex = 2;
+            this.labelRequisites.Text = "Kaspi gold: 87071082127";
+            // 
+            // labelSupport
+            // 
+            this.labelSupport.AutoSize = true;
+            this.labelSupport.Font = new System.Drawing.Font("Times New Roman", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.labelSupport.Location = new System.Drawing.Point(28, 115);
+            this.labelSupport.Name = "labelSupport";
+            this.labelSupport.Size = new System.Drawing.Size(919, 45);
+            this.labelSupport.TabIndex = 1;
+            this.labelSupport.Text = "Қолдау білдіргіңіз келсе / если хотите поддержать:";
+            // 
+            // labelAuthor
+            // 
+            this.labelAuthor.AutoSize = true;
+            this.labelAuthor.Font = new System.Drawing.Font("Times New Roman", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.labelAuthor.Location = new System.Drawing.Point(28, 23);
+            this.labelAuthor.Name = "labelAuthor";
+            this.labelAuthor.Size = new System.Drawing.Size(482, 45);
+            this.labelAuthor.TabIndex = 0;
+            this.labelAuthor.Text = "Made by Karzhaubay Ayan";
+            // 
+            // buttonClear
+            // 
+            this.buttonClear.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.buttonClear.Location = new System.Drawing.Point(418, 477);
+            this.buttonClear.Name = "buttonClear";
+            this.buttonClear.Size = new System.Drawing.Size(186, 45);
+            this.buttonClear.TabIndex = 30;
+            this.buttonClear.Text = "Тазарту";
+            this.buttonClear.UseVisualStyleBackColor = true;
+            this.buttonClear.Click += new System.EventHandler(this.buttonClear_Click);
+            // 
+            // AutoMarking
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1016, 979);
+            this.Controls.Add(this.tabControlMain);
+            this.Name = "AutoMarking";
+            this.Text = "AutoMarking";
+            this.tabControlMain.ResumeLayout(false);
+            this.tabPageMain.ResumeLayout(false);
+            this.tabPageMain.PerformLayout();
+            this.tabPageAbout.ResumeLayout(false);
+            this.tabPageAbout.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private Button buttonCalculate;
+        private TextBox textBoxMaxMark;
+        private Label labelMaxScore;
+        private Label labelPupil;
+        private Label labelScore;
+        private TextBox textBoxFiveCount;
+        private TextBox textBoxFourCount;
+        private TextBox textBoxThreeCount;
+        private TextBox textBoxClass;
+        private TextBox textBoxMarks;
+        private TextBox textBoxThree;
+        private TextBox textBoxFive;
+        private TextBox textBoxFour;
+        private Label labelQuality;
+        private TextBox textBoxQuality;
+        private TabControl tabControlMain;
+        private TabPage tabPageMain;
+        private TabPage tabPageAbout;
+        private Label labelFive;
+        private Label labelFour;
+        private Label labelThree;
+        private Label labelAuthor;
+        private Label labelSupport;
+        private Label labelRequisites;
+        private Button buttonClear;
+    }
+}
---
+++ UsefulTeacherTools/AutoMarking.cs
@@ -0,0 +1,81 @@
+﻿namespace UsefulTeacherTools
+{
+    public partial class AutoMarking : Form
+    {
+        public AutoMarking()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonCalculate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                List<string> students = textBoxClass.Text.Split("\n").ToList();
+                List<string> temp = textBoxMarks.Text.Split("\n").ToList();
+                List<int> marks = new List<int>();
+                for (int i = 0; i < temp.Count; i++)
+                {
+                    marks.Add(Convert.ToInt32(temp[i]));
+                }
+
+                double max = Convert.ToDouble(textBoxMaxMark.Text);
+
+                List<string> two = new List<string>();
+                List<string> three = new List<string>();
+                List<string> four = new List<string>();
+                List<string> five = new List<string>();
+
+                for (int i = 0; i < students.Count(); i++)
+                {
+                    if (marks[i] < 0 || marks[i] > max)
+                    {
+                        MessageBox.Show("Қателік бар!");
+                        break;
+                    }
+                    if (marks[i] >= 0.85 * max && marks[i] <= max)
+                        five.Add(students[i]);
+                    else if (marks[i] >= 0.64 * max && marks[i] < 0.85 * max)
+                        four.Add(students[i]);
+                    else if (marks[i] >= 0.4 * max && marks[i] < 0.64 * max)
+                        three.Add(students[i]);
+                    else
+                        two.Add(students[i]);
+                }
+                textBoxQuality.Text = $"{100 * (four.Count() + five.Count()) / (three.Count() + four.Count() + five.Count())}";
+
+                //textBoxTwo.Text = "0-40: " + two.Count().ToString();
+                textBoxThreeCount.Text = $"{three.Count().ToString()}";
+                textBoxFourCount.Text = $"{four.Count().ToString()}";
+                textBoxFiveCount.Text = $"{five.Count().ToString()}";
+
+                //listViewThree(two);
+                AddToTextBox(three, textBoxThree);
+                AddToTextBox(four, textBoxFour);
+                AddToTextBox(five, textBoxFive);
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show($"{ex.Message} Тізімде бос жол, дұрыс емес дерек болмағанын тексеріңіз. Максималды балл дұрыс енгізілгенін тексеріңіз.");
+            }
+        }
+        private void AddToTextBox(List<string> list, TextBox textBox)
+        {
+            foreach(var item in list)
+                textBox.Text += $"{item}\n";
+        }
+
+        private void buttonClear_Click(object sender, EventArgs e)
+        {
+            textBoxFive.Text = "";
+            textBoxFiveCount.Text = "";
+            textBoxFour.Text = "";
+            textBoxFourCount.Text = "";
+            textBoxThree.Text = "";
+            textBoxThreeCount.Text = "";
+            textBoxMarks.Text = "";
+            textBoxMaxMark.Text = "";
+            textBoxQuality.Text = "";
+        }
+    }
+}
